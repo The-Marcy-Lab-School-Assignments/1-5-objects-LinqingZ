f410e10b55a12594483487d7404d718df21dd8c3
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-04-11 17:54:14 -0400

    Done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -31,20 +31,65 @@ const carMaker = () => {
 };
 
 const weAreNotFriends = (person) => {
-
+  return person.friends.pop()
 };
+const person1 = {
+  name: 'Sara',
+  age: 30,
+  friends: ['Bob', 'Joe', 'Sally'],
+}
+// weAreNotFriends(person1)
+// console.log(person1.friends)
+
 
 const listHobbies = () => {
 };
 
-const getNextOpponent = () => {
+const getNextOpponent = (team) => {
+  if (team.matches[0] === undefined) {
+    return null
+  } else {
+    return team.matches[0].teamName
+  }
 };
+
+const fighters = {
+  name: 'Fighters',
+  sport: 'basketball',
+  wins: 3,
+  location: {
+    city: 'Bridgeport',
+    state: 'CT',
+  },
+  matches: [
+    {
+      teamName: 'Dunkaroos',
+      skill: 9,
+      wins: 12,
+    },
+    {
+      teamName: 'Space Jammers',
+      skill: 10,
+      wins: 16,
+    },
+    {
+      teamName: 'Mustangs',
+      skill: 6,
+      wins: 10,
+    },
+  ]
+}
+
+//console.log(getNextOpponent(fighters))
 
 const listAllKeys = () => {
 };
 
-const listAllValues = () => {
+const listAllValues = (obj) => {
+  return Object.values(obj)
 };
+//console.log(listAllValues(fighters))
+
 
 const convertToMatrix = () => {
 };
