8b9d2760be553cdb4f0b43f80cc9e1be6dd813a6
LinqingZ <linqing201@gmail.com>
2024-04-11 21:30:34 -0400

    push to remote orgin

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,32 +1,105 @@
-const coolGreeting = () => {
+// Q1
+const coolGreeting = (person) => {
+  // return {
+  //   name: person.name,
+  //   bio: person.bio,
+  //   age : person.age,
+  //   isCool : person.isCool
+
+  // }
+  if (person.isCool){
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`
+  }
+  return `Greetings ${person.name}, how have you been lately?`
 };
 
-const haveBirthday = () => {
+// Q2
+const haveBirthday = (person) => {
+  person.age += 1
 };
 
-const becomeSecretAgent = () => {
+// Q3
+const becomeSecretAgent = (person, spyHandle) => {
+  delete person.name;
+  person["spyHandle"] = spyHandle;
 };
 
-const carMaker = () => {
+// Q4
+const carMaker = (name, maker, year) => {
+  return {
+    name: name,
+    year: year,
+    maker: maker,
+    needsOilChange: false
+  }
 };
 
-const weAreNotFriends = () => {
+// Q5
+const weAreNotFriends = (person) => {
+  if (person && person.friends && person.friends.length >= 0) {
+    return person.friends.pop()
+  }else{
+    return person.friends
+  }
+  // return person.friends.pop()
+};
+// Q6
+const listHobbies = (person) => {
+  // if (person && person.hobbies && person.hobbies.length > 0){}
+  for (let hobby of person.hobbies){
+    console.log(`${person.name} likes ${hobby}.`)
+  }
 };
 
-const listHobbies = () => {
+// Q7
+const getNextOpponent = (team) => {
+  if (team && team.matches && team.matches.length >0 ){
+    return team.matches[0].teamName
+  }else{
+    return null
+  }
 };
 
-const getNextOpponent = () => {
+// Q8
+const listAllKeys = (anObject) => {
+  return Object.keys(anObject)
 };
 
-const listAllKeys = () => {
+// Q9
+const listAllValues = (anObject) => {
+  return Object.values(anObject)
 };
 
-const listAllValues = () => {
+// Q10
+const convertToMatrix = (objects) => {
+  let output = []
+  if (objects.length>0){
+    // console.log(`object inside`)
+    output.push(Object.keys(objects[0]))
+    // console.log(Object.keys(objects[0]))
+    for (let obj of objects){
+      output.push(Object.values(obj))
+    }
+  return output
+  }else{
+    return output
+  }
 };
 
-const convertToMatrix = () => {
-};
+const users = [
+  {
+    name: 'Sara',
+    age: 30,
+    bio: 'What a legend',
+  },
+  {
+    name: 'Bob',
+    age: 30,
+    bio: "Kind of mean if we're being honest",
+  },
+];
+
+convertToMatrix(users)
 
 module.exports = {
   coolGreeting,
